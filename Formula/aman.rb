97aee98115b4af5283ba42f1e105d4b126422433
goreleaserbot <goreleaser@carlosbecker.com>
2020-12-27 14:43:34 +0900

    Brew formula update for aman version v0.2.4

--- Formula/aman.rb
+++ Formula/aman.rb
@@ -2,16 +2,16 @@
 class Aman < Formula
   desc "aman"
   homepage "https://github.com/naruhiyo/aman"
-  version "0.2.3"
+  version "0.2.4"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/naruhiyo/aman/releases/download/v0.2.3/aman_darwin_amd64.tar.gz"
-    sha256 "e0363003e5daafb6f20fd33e811932cb8e8bc909a8fee533130eea5dbc5d07cc"
+    url "https://github.com/naruhiyo/aman/releases/download/v0.2.4/aman_darwin_amd64.tar.gz"
+    sha256 "fe290df479829d7f6728d6aa2bd3577e5b9d087baae5380b2bfb243bffabe047"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/naruhiyo/aman/releases/download/v0.2.3/aman_linux_amd64.tar.gz"
-    sha256 "4dfd1547d2102dd6453ddc9f7fe506d1be5ac8c8666983bc1df68e487fc26a00"
+    url "https://github.com/naruhiyo/aman/releases/download/v0.2.4/aman_linux_amd64.tar.gz"
+    sha256 "a7e651fd12888b5a494c363ef86c3099add942142d845590516b9f8ab7acb577"
   end
 
   def install
